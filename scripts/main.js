30cd13a568ba19dc341a80bed6c5d065c6654eb6
C215714N <CristianDRacedo@hotmail.com>
2020-10-14 12:00:01 -0300

    menu hamburguesa

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
 document.querySelector('.#menu').addEventListener(
 	'click', () => {
 		document.querySelector('.menu').classList.toggle('openMenu')
+		if(true){
+			document.querySelector('#menu').innerHTML = '&times;'
+		} else {
+			document.querySelector('#menu').innerHTML = '&equiv;'
+		}
 	}
 )
 
